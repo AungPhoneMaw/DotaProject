cff98d67a5e3a5b0685614cdef60481c8d32946c
AungPhoneMaw <khant09.kz@gmail.com>
2025-12-13 05:47:01 +0100

    add atomic json dump in pipeline.py. and some fixes

--- main.py
+++ main.py
@@ -4,34 +4,3 @@ import ast
 import pprint 
 import json
 import time
-
-BATCH = 50
-SLEEP = 60
-
-heroes_df = pd.read_csv("heroes.csv")
-
-HID = heroes_df["id"].to_list()
-print(HID)
-matches_df = pd.read_csv("matches.csv")
-matches_df["radiant_team"] = matches_df["radiant_team"].apply(ast.literal_eval)
-matches_df["dire_team"] = matches_df["dire_team"].apply(ast.literal_eval)
-print(matches_df)
-"""
-the goal is to get a dict where keys are hero id and values are match id where 
-the hero is picked
-"""
-hero_match_dict = {hid: [] for hid in HID}
-for _,row in matches_df.iterrows():
-    match_id = row["match_id"]
-    for hid in row["radiant_team"] + row["dire_team"]:
-        if hid != 0:
-            hero_match_dict[hid].append(match_id)
-
-with open("hero_match_dict.json", "w") as f:
-    json.dump(hero_match_dict, f)
-
-hero_match_count = {}
-for key in hero_match_dict.keys():
-    hero_match_count[key] = len(hero_match_dict[key])
-
-pprint.pprint(hero_match_count)
--- pipline.py
+++ pipline.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pprint 
 import json
 import pickle
-import time
+import time, os, tempfile
 import logging
 
 """
@@ -31,22 +31,23 @@ with open("hero_id_list.pkl", "rb") as f:
     logger.info("loaded hero_id_list.pkl")
 
 #data format to be collected for each hero
-hero_data_dict = {hid: {"hero_damage":[],
-                        "tower_damage":[],
-                        "healing":[],
-                        "team_hero_damage":[],
-                        "team_tower_damage":[],
-                        "team_healing":[],
-                        "total_gold":[],
-                        "total_xp":[],
-                        "hdpm":[],
-                        "tdpm":[],
-                        "hpm":[],
-                        "thdpm":[],
-                        "ttdpm":[],
-                        "thpm":[],
-                        "gpm":[],
-                        "xpm":[]} for hid in HID}
+def load_hero_data_dict(HID):
+    """ load hero data dict from existing hero_data.json if exists """
+
+    header = ["hero_damage","tower_damage","healing",
+              "team_hero_damage","team_tower_damage","team_healing", "total_gold", "total_xp",
+              "hdpm","tdpm","hpm","thdpm","ttdpm","thpm","gpm","xpm"]
+    if not os.path.exists("hero_data.json"):
+        return {
+            hid: {k:[] for k in header} for hid in HID
+        }
+    #load existing data
+    with open("hero_data.json", "r") as f:
+        existing_data = json.load(f)
+        logger.info("Loaded existing hero_data.json")
+        return existing_data
+    
+hero_data_dict = load_hero_data_dict(HID)
 logger.info("Initialized hero_data_dict")
 #retrieving match ids
 matches = pd.read_csv("matches.csv")
@@ -82,12 +83,16 @@ def safe_get(match_id, retries = 5, delay = 5):
     logger.error(f"Failed to retrieve match id {match_id} after {retries} attempts.", exc_info=True)
     return None
 
+def atomic_json_dump(path, data):
+    with tempfile.NamedTemporaryFile("w", delete=False) as tf:
+        json.dump(data, tf, indent=2)
+        temp_path = tf.name
+    os.replace(temp_path, path)
+
 def save_progress(data, last_batch):
     """this function saves the progress of data collection"""
-    with open("hero_data.json", "w") as f:
-        json.dump(data, f)
-    with open("progress.json", "w") as f:
-        json.dump({"last_batch": last_batch}, f)
+    atomic_json_dump("hero_data.json", data)
+    atomic_json_dump("progress.json", {"last_batch": last_batch})
     logger.info(f"Progress saved. batch: {last_batch}")
 
 def load_progress():
@@ -100,69 +105,68 @@ def load_progress():
     except FileNotFoundError:
         logger.info("No progress file found, starting from the beginning.")
         return -1
-last_loaded_batch = load_progress()
-for batch_index, batch_start in enumerate(range(0, len(match_ids), BATCH)):
-    if batch_index <= last_loaded_batch:
-        print(f"Skipping batch {batch_index}...")
-        logger.info(f"Skipping batch {batch_index}...")
-        continue
-    for match_id in match_ids[batch_start:batch_start+BATCH]:
-        match = fetch_match(match_id)
-        if match is None:
-            logger.error(f"Skipping match id {match_id} due to retrieval failure.")
+
+def hero_data():
+    """pipeline to collect hero data from matches and stoere it in hero_data.json"""
+    last_loaded_batch = load_progress()
+    for batch_index, batch_start in enumerate(range(0, len(match_ids), BATCH)):
+        if batch_index <= last_loaded_batch:
+            print(f"Skipping batch {batch_index}...")
+            logger.info(f"Skipping batch {batch_index}...")
             continue
-        duration = match["duration"]
-        print("Match Duration: ", duration)
-        players = match["players"]
-        # Radiant/dire totals
-        team_totals = {
-            "radiant": {"hero": 0, "tower": 0, "heal": 0},
-            "dire": {"hero": 0, "tower": 0, "heal": 0}
-        }
-
-        for p in players:
-            side = "radiant" if p["isRadiant"] else "dire"
-            team_totals[side]["hero"] += p["hero_damage"]
-            team_totals[side]["tower"] += p["tower_damage"]
-            team_totals[side]["heal"] += p["hero_healing"]
-            logger.debug(f"Updated {side} totals: {team_totals[side]}")
-
-        # assign values
-        for p in players:
-            hid = p["hero_id"]
-            if hid == 0:
-                logging.debug(f"Skipping invalid hero_id 0 in match {match_id}")
+        for match_id in match_ids[batch_start:batch_start+BATCH]:
+            match = fetch_match(match_id)
+            if match is None:
+                logger.error(f"Skipping match id {match_id} due to retrieval failure.")
                 continue
+            duration = match["duration"]
+            print("Match Duration: ", duration)
+            players = match["players"]
+            # Radiant/dire totals
+            team_totals = {
+                "radiant": {"hero": 0, "tower": 0, "heal": 0},
+                "dire": {"hero": 0, "tower": 0, "heal": 0}
+            }
 
-            side = "radiant" if p["isRadiant"] else "dire"
-            t = team_totals[side]
+            for p in players:
+                side = "radiant" if p["isRadiant"] else "dire"
+                team_totals[side]["hero"] += p["hero_damage"]
+                team_totals[side]["tower"] += p["tower_damage"]
+                team_totals[side]["heal"] += p["hero_healing"]
+                logger.debug(f"Updated {side} totals: {team_totals[side]}")
 
-            dmin = duration / 60
+            # assign values
+            for p in players:
+                hid = p["hero_id"]
+                if hid == 0:
+                    logging.debug(f"Skipping invalid hero_id 0 in match {match_id}")
+                    continue
 
-            hero_data_dict[hid]["hero_damage"].append(p["hero_damage"])
-            hero_data_dict[hid]["tower_damage"].append(p["tower_damage"])
-            hero_data_dict[hid]["healing"].append(p["hero_healing"])
-            hero_data_dict[hid]["team_hero_damage"].append(t["hero"])
-            hero_data_dict[hid]["team_tower_damage"].append(t["tower"])
-            hero_data_dict[hid]["team_healing"].append(t["heal"])
-            hero_data_dict[hid]["total_gold"].append(p["total_gold"])
-            hero_data_dict[hid]["total_xp"].append(p["total_xp"])
+                side = "radiant" if p["isRadiant"] else "dire"
+                t = team_totals[side]
 
-            hero_data_dict[hid]["hdpm"].append(p["hero_damage"] / dmin)
-            hero_data_dict[hid]["tdpm"].append(p["tower_damage"] / dmin)
-            hero_data_dict[hid]["hpm"].append(p["hero_healing"] / dmin)
-            hero_data_dict[hid]["thdpm"].append(t["hero"] / dmin)
-            hero_data_dict[hid]["ttdpm"].append(t["tower"] / dmin)
-            hero_data_dict[hid]["thpm"].append(t["heal"] / dmin)
-            hero_data_dict[hid]["gpm"].append(p["total_gold"] / dmin)
-            hero_data_dict[hid]["xpm"].append(p["total_xp"] / dmin)
-            logger.debug(f"Appended data for hero_id {hid} in match {match_id}")
-    save_progress(hero_data_dict, batch_index)
+                dmin = duration / 60
 
-    print("waiting 60s to avoid rate limit...")
-    logger.info("Batch completed, sleeping to avoid rate limit...")
-    time.sleep(SLEEP)
-pprint.pprint(hero_data_dict)
-#saving hero data dict to json
-with open("hero_data.json", "w") as f:
-    json.dump(hero_data_dict, f)
+                hero_data_dict[hid]["hero_damage"].append(p["hero_damage"])
+                hero_data_dict[hid]["tower_damage"].append(p["tower_damage"])
+                hero_data_dict[hid]["healing"].append(p["hero_healing"])
+                hero_data_dict[hid]["team_hero_damage"].append(t["hero"])
+                hero_data_dict[hid]["team_tower_damage"].append(t["tower"])
+                hero_data_dict[hid]["team_healing"].append(t["heal"])
+                hero_data_dict[hid]["total_gold"].append(p["total_gold"])
+                hero_data_dict[hid]["total_xp"].append(p["total_xp"])
+
+                hero_data_dict[hid]["hdpm"].append(p["hero_damage"] / dmin)
+                hero_data_dict[hid]["tdpm"].append(p["tower_damage"] / dmin)
+                hero_data_dict[hid]["hpm"].append(p["hero_healing"] / dmin)
+                hero_data_dict[hid]["thdpm"].append(t["hero"] / dmin)
+                hero_data_dict[hid]["ttdpm"].append(t["tower"] / dmin)
+                hero_data_dict[hid]["thpm"].append(t["heal"] / dmin)
+                hero_data_dict[hid]["gpm"].append(p["total_gold"] / dmin)
+                hero_data_dict[hid]["xpm"].append(p["total_xp"] / dmin)
+                logger.debug(f"Appended data for hero_id {hid} in match {match_id}")
+        save_progress(hero_data_dict, batch_index)
+
+        print("waiting 60s to avoid rate limit...")
+        logger.info("Batch completed, sleeping to avoid rate limit...")
+        time.sleep(SLEEP)
